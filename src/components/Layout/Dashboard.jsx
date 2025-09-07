185afd2dcaf89a792b3f146a7bb91d96f5721d6c
Muhammad Azwin Rifai <azwinrifai321@gmail.com>
2025-09-07 22:46:19 +0700

    add sky background Dashboard
    -

--- src/components/Layout/Dashboard.jsx
+++ src/components/Layout/Dashboard.jsx
@@ -1,5 +1,6 @@
 import Header from "../Fragments/Header";
 import Sidebar from "../Fragments/Sidebar";
+import skyBackground from "../../assets/Sky Background.png";
 
 export default function Dashboard({ children }) {
   return (
@@ -7,7 +8,12 @@ export default function Dashboard({ children }) {
       <Sidebar />
       <div className="flex-1 flex flex-col">
         <Header />
-        <main className="p-6 bg-[#F7F5EF] min-h-screen ml-56 mt-10">{children}</main>
+        <main
+          className="p-6 min-h-screen ml-56 mt-10 bg-cover bg-center bg-no-repeat"
+          style={{ backgroundImage: `url(${skyBackground})` }}
+        >
+          {children}
+        </main>
       </div>
     </div>
   );
